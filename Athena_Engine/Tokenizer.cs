7d382fb6e362bd76ba53cc80083867d8566b826d
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2021-01-16 17:14:48 +0000

    Now it adds the variable to the node list

--- Athena_Engine/Tokenizer.cs
+++ Athena_Engine/Tokenizer.cs
@@ -27,7 +27,7 @@ namespace Athena_Engine {
 				if (Char.IsLetter(s[i])) //Check if current letter is a string and if it is creates a Node with it
                 {
 					string variable = s[i].ToString();
-					for (int e = i + 1; e<= (s.Length - 1); e++)
+					for (int e = i + 1; e<= (s.Length - 1); e++) //Now get the rest of chars of the variable and get the next element to tokenize
                     {
 						if (Char.IsLetter(s[e]))
                         {
@@ -37,7 +37,11 @@ namespace Athena_Engine {
 							len_to_jump = e + 1;
                         }
                     }
-					Console.WriteLine(variable);
+					//TODO:Check if variable is different than exclusive ones if not, create an exception
+					Node nv = new Node(); //Finally create a node for this variable and add it to the list
+					nv.t = Types.Variable;
+					nv.var = variable;
+					list_of_nodes.Add(nv);
 				}
 			}
 		}
