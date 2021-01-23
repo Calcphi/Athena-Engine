c5af443995d18d548f3a8172d7e17ee5e59dbd2c
Luís Duarte <luis.duarte.2003.13@ghotmail.com>
2021-01-23 18:39:14 +0000

    Added the parser to the program tests

--- Console tests/Program.cs
+++ Console tests/Program.cs
@@ -12,11 +12,18 @@ namespace Console_tests
             {
                 Console.Write("Equation:");
                 Tokenizer t = new Tokenizer();
+                Parser p = new Parser();
                 List<Node> nodes = t.Tokenize(Console.ReadLine());
                 foreach (Node n in nodes)
                 {
-                    Console.WriteLine(n.t + " " + n.op + " " + n.value + " " + n.var + "\n" );
+                    Console.WriteLine(n.t + " " + n.op + " " + n.value + " " + n.var + " " + n.f + "\n" );
                 }
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                Node nf = p.Parse(nodes);
+                Console.WriteLine(nf.t + " " + nf.op + " " + nf.value + " " + nf.var + " " + nf.f + "\n");
                 Console.WriteLine("\n");
             }
 
