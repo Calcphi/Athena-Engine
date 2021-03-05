bab68abba0ea809230c2ead2394ec8bb1a0902d7
Luís Duarte <luis.duarte.2003.13@ghotmail.com>
2021-03-05 18:51:30 +0000

    Forgot to fix the same bug in rule 5 but in the division side of things

--- Athena_Engine/Simplifier.cs
+++ Athena_Engine/Simplifier.cs
@@ -384,10 +384,10 @@ namespace Athena_Engine
                     //we check if the variable is there
                     if (CheckForVariable(numerator_exponent_base, 0, 2) == true && CheckForVariable(denominator_exponent_base, 0, 2) == true)
                     {
-                        if (numerator_exponent_base.exp[1].var == denominator_exponent_base.exp[1].var)
+                        if (numerator_exponent_base.var == denominator_exponent_base.var)
                         {
                             n = new Node() { t = Types.Operator, op = Operators.Exponent, priority_value = n.priority_value };
-                            n.exp[0] = numerator_exponent_base.exp[1];
+                            n.exp[0] = numerator_exponent_base;
                             n.exp[1] = new Node() { t = Types.Operator, op = Operators.Subtraction, priority_value = n.priority_value + 1 };
                             n.exp[1].exp[0] = numerator_exponent_exponent;
                             n.exp[1].exp[1] = denominator_exponent_exponent;
