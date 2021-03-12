bee7f296dffb1400adfc157390699bf701af4756
Luís Duarte <luis.duarte.2003.13@ghotmail.com>
2021-03-12 15:55:58 +0000

    Added the solver tests

--- Tests/SolverTesting.cs
+++ Tests/SolverTesting.cs
@@ -7,9 +7,74 @@ namespace Tests
     [TestClass]
     public class SolverTesting
     {
+        Tokenizer t = new Tokenizer();
+        Parser p = new Parser();
+        Solver s = new Solver();
+
         [TestMethod]
-        public void TestMethod1()
+        
+        public void SolverAdditionTest()
         {
+            for (int i = 0; i <= 100; i++)
+            {
+                Random r = new Random();
+                int i1 = r.Next(1, 1000000);
+                int i2 = r.Next(1, 1000000);
+
+                Assert.AreEqual((double)(i1 + i2), s.Solve(p.Parse(t.Tokenize(i1.ToString() + "+" + i2.ToString()))));
+            }
+        }
+
+        [TestMethod]
+        public void SolverSubtractionTest()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Random r = new Random();
+                int i1 = r.Next(1, 1000000);
+                int i2 = r.Next(1, 1000000);
+
+                Assert.AreEqual((double)(i1 - i2), s.Solve(p.Parse(t.Tokenize(i1.ToString() + "-" + i2.ToString()))));
+            }
+        }
+
+        [TestMethod]
+        public void SolverMultiplicationTest()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Random r = new Random();
+                int i1 = r.Next(1, 1000000);
+                int i2 = r.Next(1, 1000000);
+
+                Assert.AreEqual((double)i1 * i2, s.Solve(p.Parse(t.Tokenize(i1.ToString() + "*" + i2.ToString()))));
+            }
+        }
+
+        [TestMethod]
+        public void SolverDivisionTest()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Random r = new Random();
+                int i1 = r.Next(1, 1000000);
+                int i2 = r.Next(1, 1000000);
+
+                Assert.AreEqual((double)i1 / i2, s.Solve(p.Parse(t.Tokenize(i1.ToString() + "/" + i2.ToString()))));
+            }
+        }
+
+        [TestMethod]
+        public void SolverExponentTest()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Random r = new Random();
+                int i1 = r.Next(1, 1000000);
+                int i2 = r.Next(1, 1000000);
+
+                Assert.AreEqual((double)Math.Pow(i1, i2), s.Solve(p.Parse(t.Tokenize(i1.ToString() + "^" + i2.ToString()))));
+            }
         }
     }
 }
