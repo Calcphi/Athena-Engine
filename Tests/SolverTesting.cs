fdc91219a55c43787a1f00621e3f341bcf3bd887
Luís Duarte <luis.duarte.2003.13@ghotmail.com>
2021-03-12 15:23:10 +0000

    Added testing to Athena_Engine

---
+++ Tests/SolverTesting.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Athena_Engine;
+
+namespace Tests
+{
+    [TestClass]
+    public class SolverTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
